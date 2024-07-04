cabdd206557408e237ca974878562aec1bcdc045
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-04 15:53:59 +0530

    main files

---
+++ constants.py
@@ -0,0 +1,4 @@
+APPOINTMENT_SERVICE_URL = 'http://localhost:8001/api/appointments/'
+PATIENT_SERVICE_URL = 'http://localhost:8002/api/patients/'
+DOCTOR_SERVICE_URL = 'http://localhost:8003/api/doctors/'
+INFRASTRUCTURE_SERVICE_URL = 'http://localhost:8004/api/infrastructure/'
