6a49e065b8522ee82202cfa5f315e047d503fabd
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-06 20:22:58 +0530

    added docker and modified env file

--- apps/gateway/urls.py
+++ apps/gateway/urls.py
@@ -3,11 +3,11 @@ from .views import AppointmentProxyView, PatientProxyView, DoctorProxyView, Infr
 
 urlpatterns = [
     path('appointments/', AppointmentProxyView.as_view(), name='appointments_proxy'),
-    path('appointments/<str:appointment_id>/', AppointmentProxyView.as_view(), name='appointment_detail_proxy'),
+    path('appointments/<int:appointment_id>/', AppointmentProxyView.as_view(), name='appointment_detail_proxy'),
     path('patients/', PatientProxyView.as_view(), name='patients_proxy'),
-    path('patients/<str:patient_id>/', PatientProxyView.as_view(), name='patient_detail_proxy'),
+    path('patients/<int:patient_id>/', PatientProxyView.as_view(), name='patient_detail_proxy'),
     path('doctors/', DoctorProxyView.as_view(), name='doctors_proxy'),
-    path('doctors/<str:doctor_id>/', DoctorProxyView.as_view(), name='doctor_detail_proxy'),
+    path('doctors/<int:doctor_id>/', DoctorProxyView.as_view(), name='doctor_detail_proxy'),
     path('infrastructure/', InfrastructureProxyView.as_view(), name='infrastructure_proxy'),
     path('infrastructure/<int:infrastructure_id>/', InfrastructureProxyView.as_view(), name='infrastructure_detail_proxy'),
 ]
